43b64d45694f52f84f0e11556b1ed00d0c71cc20
Jaime Cabrera <jycabrerap@unl.edu.ec>
2022-02-24 14:51:02 -0500

    delete firabase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,1 @@
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCiX-BYgjMPXUayQVOWzUq6-wKaFYmxBaw',
-//   authDomain: 'restaurant-f44fc.firebaseapp.com',
-//   projectId: 'restaurant-f44fc',
-//   storageBucket: 'restaurant-f44fc.appspot.com',
-//   messagingSenderId: '140978050868',
-//   appId: '1:140978050868:web:d19988cd3acfcd33457761',
-// };
-const firebaseConfig = {
-  apiKey: 'AIzaSyCiX-BYgjMPXUayQVOWzUq6-wKaFYmxBaw',
-  authDomain: 'restaurant-f44fc.firebaseapp.com',
-  projectId: 'restaurant-f44fc',
-  storageBucket: 'restaurant-f44fc.appspot.com',
-  messagingSenderId: '140978050868',
-  appId: '1:140978050868:web:d19988cd3acfcd33457761',
-};
 
-export default firebaseConfig;
